300782519fb2a68cfb00942de89ccdd7e5bce2f2
David Sorensen <teardownthismall@gmail.com>
2022-08-09 15:17:19 -0400

    adjusted module exports

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ["firebasestorage.googleapis.com", "shandonblack.netlify.app"],
+        domains: [
+            "firebasestorage.googleapis.com",
+            "shandonblack.netlify.app",
+            "netlify.app",
+            "googleapis.com",
+        ],
+        formats: ["image/webp", "image/avif"],
     },
     target: "serverless",
 };
